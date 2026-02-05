beeb138943fbc55dd8ba80f0008076e558bb488e
shanto <shantohasibulhasan379@gmail.com>
2026-02-06 04:22:13 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,5 @@
+// console.log(5 > 10)
+// console.log(5 < 10)
+// console.log(5 == 10)
+
+
---
+++ if.js
@@ -0,0 +1,13 @@
+
+// if (3 < 10){
+//     console.log('i have a smaller value')
+// }else{
+//     console.log('i have a biger value')
+// }
+
+var biriyaniPrice = 300;
+if(biriyaniPrice < 300){
+    console.log('mama give some biriyani')
+}else{
+console.log('mama tmr tk short ase tk niye aso')
+}
