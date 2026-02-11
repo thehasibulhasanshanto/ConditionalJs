6c3a0197e85a9241a917cb9ebaa05100f66723ed
shanto <shantohasibulhasan379@gmail.com>
2026-02-12 01:32:08 +0600

    added PracticeTask

---
+++ practiceTask.js
@@ -0,0 +1,24 @@
+const burgerPrice = 501
+
+if (burgerPrice > 500){
+    // console.log('You will get free coke')
+}else{
+    // console.log('You will be pay for coke')
+}
+
+const mark = -500
+
+if (mark >=90 && mark <=100){
+    // console.log('You got GPA A')
+}else if (mark <=89 && mark >=80){
+    // console.log('You got GPA B')
+}else if (mark <=79 && mark >=70){
+    // console.log('You got GPA C')
+}else if (mark <=69 && mark >=60){
+    // console.log('You got GPA D')
+}else if (mark <0 && mark >100){
+    // console.log('Invalid Result')
+}
+else{
+    // console.log('Youre Fail')
+}
